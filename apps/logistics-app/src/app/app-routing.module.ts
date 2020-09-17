f18bdfd0a385b9496fff1ef017b24d4e89797654
Ramil Kutov <moreleaks@outlook.com>
2020-09-17 22:54:46 +0500

    feat(table): add route transition animation

--- apps/logistics-app/src/app/app-routing.module.ts
+++ apps/logistics-app/src/app/app-routing.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { LogisticsListComponent } from './logistics-list/logistics-list.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { BasicComponent } from './basic/basic.component';
 
 const routes: Routes = [
-  { path: 'list', component: LogisticsListComponent },
-  { path: '', component: BasicComponent, pathMatch: 'full' },
-  { path: '**', component: NotFoundComponent },
+  {
+    path: 'list',
+    component: LogisticsListComponent,
+    data: { animation: 'ListPage' },
+  },
+  {
+    path: '',
+    component: BasicComponent,
+    pathMatch: 'full',
+    data: { animation: 'HomePage' },
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    data: { animation: 'NotFoundRoute' },
+  },
 ];
 
 @NgModule({
--- apps/logistics-app/src/app/app.component.ts
+++ apps/logistics-app/src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { routerAnimation } from './animations/route-animations';
 
 @Component({
   selector: 'logistics-org-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [routerAnimation],
 })
-export class AppComponent {}
+export class AppComponent {
+  public handleRoute(outlet: RouterOutlet) {
+    return outlet?.activatedRouteData?.animation;
+  }
+}
